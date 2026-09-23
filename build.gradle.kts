b3835ef25609597ab40d2ed52822775cc9caa54e
Lunasa <me@lunasa.dev>
2026-09-24 03:38:36 +0530

    build: publish to the clover releases repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     kotlin("multiplatform") version "2.4.10"
+    `maven-publish`
 }
 
 group = "gg.sona"
-version = "1.0-SNAPSHOT"
+version = providers.gradleProperty("mslVersion").getOrElse("1.0.0")
 
 repositories {
     mavenCentral()
@@ -25,12 +26,29 @@ kotlin {
     macosArm64()
 
     compilerOptions {
+        allWarningsAsErrors.set(true)
         freeCompilerArgs.add("-Xexpect-actual-classes")
     }
 
     sourceSets {
         commonTest.dependencies {
             implementation(kotlin("test"))
+        }
+    }
+}
+
+fun setting(property: String, environment: String): String? =
+    (providers.gradleProperty(property).orNull ?: providers.environmentVariable(environment).orNull)?.takeIf { it.isNotBlank() }
+
+publishing {
+    repositories {
+        maven {
+            name = "Clover"
+            url = uri("https://maven.cloverclient.com/releases")
+            credentials {
+                username = setting("cloverUsername", "CLOVER_MAVEN_USERNAME")
+                password = setting("cloverPassword", "CLOVER_MAVEN_PASSWORD")
+            }
         }
     }
 }
